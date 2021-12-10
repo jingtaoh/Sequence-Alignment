4cbe19cf1708508e18d60bbc972018b92dc7796f
Jingtao Huang <jingtaoh@usc.edu>
2021-12-10 15:15:06 -0800

    Update 1942568982_2697735160_basic.sh

--- 1942568982_2697735160_basic.sh
+++ 1942568982_2697735160_basic.sh
@@ -11,3 +11,6 @@ cmake --build . --target basic
 # Run
 # output: ../data/output_basic.txt
 ./basic ../data/input.txt
+
+# To output with time and memory, comment the line above and uncomment the line below
+# /usr/bin/time -a -f "%e\n%M" -o ../data/output_basic.txt ./basic ../data/input.txt
