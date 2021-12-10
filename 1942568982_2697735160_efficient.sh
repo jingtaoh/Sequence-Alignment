55e63b3f0a55f5d0ee85584f264896978faa2028
Jingtao Huang <jingtaoh@usc.edu>
2021-12-10 15:16:31 -0800

    Update 1942568982_2697735160_efficient.sh

--- 1942568982_2697735160_efficient.sh
+++ 1942568982_2697735160_efficient.sh
@@ -11,3 +11,6 @@ cmake --build . --target efficient
 # Run
 # output: ../data/output_efficient.txt
 ./efficient ../data/input.txt
+
+# To output with time and memory, comment the line above and uncomment the line below
+# /usr/bin/time -a -f "%e\n%M" -o ../data/output_efficient.txt ./efficient ../data/input.txt
